5644f6f2969c5067dc52c259395222ae30f09f58
atakanbalta <atakansahinbalta13@gmail.com>
2020-04-05 17:32:27 +0300

    Toplama uygulaması yazıldı

---
+++ HesaplamaApp/src/main/java/tr/medipol/yazilim/HesaplamaApp.java
@@ -0,0 +1,22 @@
+package tr.medipol.yazilim;
+
+public class HesaplamaApp {
+
+	public static void main(String[] args) {
+		
+		Hesap toplama = new Toplama();
+		String sonuc =toplama.sonucHesap("100","10000");
+		System.out.println(sonuc);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
